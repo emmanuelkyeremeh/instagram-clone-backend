b46b32a9410f325dad43b52286ffb13eb07dd8e1
Emmanuel Kyeremeh <josephkyeremeh53@gmail.com>
2021-03-30 22:44:38 +0000

    changed image destination route

--- routes/PostRoutes.js
+++ routes/PostRoutes.js
@@ -9,7 +9,7 @@ const PostRouter = express.Router();
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "https://instagram-clone-xi.vercel.app/");
+    cb(null, "https://instagram-clone-xi.vercel.app/public/");
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
