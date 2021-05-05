569b22ad3e9c38bca4c6228a3e86292075fe6588
Emmanuel Kyeremeh <josephkyeremeh53@gmail.com>
2021-05-05 20:21:53 +0000

    added cors permissions

--- server.js
+++ server.js
@@ -19,7 +19,12 @@ const PORT = process.env.PORT;
 const app = express();
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
